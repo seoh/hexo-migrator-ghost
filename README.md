3678f41b57e9d4fe7778fe14277b12e17f44936b
Seoh Char <devthewild@gmail.com>
2014-11-03 14:51:22 +0900

    change RSS to Ghost

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
-# RSS migrator
+# Ghost migrator
 
-Migrate your blog from RSS to [Hexo].
+Migrate your blog from Ghost to [Hexo].
 
 ## Install
 
 ``` bash
-$ npm install hexo-migrator-rss --save
+$ npm install hexo-migrator-ghost --save
 ```
 
 ## Usage
 
-Execute the following command after installed. `source` is the file path or URL of RSS.
+Execute the following command after installed. `source` is the file path or URL of Ghost.
 
 ``` bash
-$ hexo migrate rss <source>
+$ hexo migrate ghost <source>
 ```
 
 [Hexo]: http://zespia.tw/hexo
+
+## Recommend Configuration
+
+``` yml 
+permalink: :slug/
+```
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var FeedParser = require('feedparser'),
   request = require('request'),
   fs = require('fs');
 
-hexo.extend.migrator.register('rss', function(args, callback){
+hexo.extend.migrator.register('ghost', function(args, callback){
   var source = args._.shift();
 
   if (!source){
     var help = [
-      'Usage: hexo migrate rss <source>',
-      '',
-      'For more help, you can check the docs: http://hexo.io/docs/migration.html'
+      'Usage: hexo migrate ghost <source>'
+      // '',
+      // 'For more help, you can check the docs: http://hexo.io/docs/migration.html'
     ];
 
     console.log(help.join('\n'));
@@ -21,6 +21,12 @@ hexo.extend.migrator.register('rss', function(args, callback){
   var log = hexo.log,
     post = hexo.post,
     stream;
+
+  if (source[source.length-1] !== '/') {
+    source = source + '/rss';
+  } else {
+    source = source + 'rss';
+  }
 
   // URL regular expression from: http://blog.mattheworiordan.com/post/13174566389/url-regular-expression-for-links-with-or-without-the
   if (source.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/)){
@@ -47,6 +53,7 @@ hexo.extend.migrator.register('rss', function(args, callback){
     while (item = stream.read()){
       posts.push({
         title: item.title,
+        slug: item.link.substring(1, item.link.length-1),
         date: item.date,
         tags: item.categories,
         content: tomd(item.description)
